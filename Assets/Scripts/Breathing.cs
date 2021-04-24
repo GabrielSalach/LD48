6e8f9fc36357b1e109a4e4bd38096f5a1e8c75c1
GabrielSalach <gabsalach@orange.fr>
2021-04-24 20:57:17 +0200

    bulles tavu

---
+++ Assets/Scripts/Breathing.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathing : MonoBehaviour
+{
+    [SerializeField] GameObject particlePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(Bubbles());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Bubbles()
+    {
+        while(true)
+        {
+            Instantiate(particlePrefab, transform);
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
