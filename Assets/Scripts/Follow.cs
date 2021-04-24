f2c498a4667dfdad3df5d424121754eb9c139422
Benjamin Villa <ywqqqq@gmail.com>
2021-04-24 23:25:19 +0200

    followup

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -5,31 +5,59 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
 
-	Vector
+	bool isFollowing;
+	GameObject character;
+	Vector3 characterPosition;
+	Rigidbody2D creature;
+	Vector2 velocityVector;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    	character = GameObject.FindGameObjectWithTag("Character");
+    	characterPosition = character.transform.position;
+    	creature = gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>();
+    	velocityVector = new Vector2(0,0);
+    	isFollowing = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    	characterPosition = character.transform.position;
+    	if(isFollowing) {
+    		follow();
+    	}
         
     }
 
     void OnTriggerEnter2D(Collider2D other) {
 
     	if(other.tag == "Character_HitBox") {
-    		follow(other.transform.gameObject);
+    		isFollowing = true;
     	}
 
     }
 
-    void follow(GameObject other) {
+    void OnTriggerExit2D(Collider2D other) {
 
+    	if(other.tag == "Character_HitBox") {
+    		isFollowing = false;
+    	}
 
+    	creature.velocity = new Vector2(0,0);
+
+    }
+
+    void follow() {
+
+    	velocityVector.x = character.transform.position.x - transform.position.x;
+    	velocityVector.y = character.transform.position.y - transform.position.y;
+
+    	creature.velocity += velocityVector / 20;
 
     }
 
